d2af3923ee8f94f8290297ccb62a6a818160f6d2
Leandro Bezerra de Andrade <leandro.bezerra@caeplab.com.br>
2018-12-27 14:53:23 -0200

    services

---
+++ services/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent,
+    CriarCursoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CursosService],                           // Providers fornecem acesso a serviços
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
